2feff70ee558b4598f0b298a37112c6f34c0aa52
One234Fi <54912519+One234Fi@users.noreply.github.com>
2023-05-28 20:43:42 -0500

    Made a preliminary parallelized version of stringVectorizer

---
+++ src/nlp/ParallelizedStringVectorizer.java
@@ -0,0 +1,39 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package nlp;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ * 
+ * @author ethan
+ */
+public class ParallelizedStringVectorizer extends StringVectorizer {
+    @Override
+    public double[][] getVectorizedData(String[] contentSentences, String[] wordDictionary) {
+        double[][] vectorizedSentences = new double[contentSentences.length][wordDictionary.length];
+        ExecutorService executor = Executors.newCachedThreadPool();
+        for (int i = 0; i < contentSentences.length; i++) {
+            //vectorizedSentences[i] = getVectorizedString(contentSentences[i], wordDictionary);
+            executor.execute(new VectorizerThread(vectorizedSentences, i, contentSentences[i], wordDictionary, this));
+        }
+        executor.shutdown();
+        try {
+            //periodically print a message and block until all the threads are done
+            boolean awaitTermination = executor.awaitTermination(30, TimeUnit.SECONDS);
+            while (!executor.isTerminated()) {
+                System.out.println("Waiting for vectorizer threads to finish...");
+                awaitTermination = executor.awaitTermination(30, TimeUnit.SECONDS);
+            }
+        } catch (InterruptedException ex) {
+            Logger.getLogger(ParallelizedStringVectorizer.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return vectorizedSentences;
+    }
+}
---
+++ src/nlp/VectorizerThread.java
@@ -0,0 +1,34 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package nlp;
+
+/**
+ * This class might be stupid/bad practice but I'm trying it anyway
+ * @author ethan
+ */
+public class VectorizerThread implements Runnable {
+    //reference to array, index to modify, the string to vectorize
+    private double[][] reference;
+    private int indexToModify;
+    private String string;
+    private String[] wordDictionary;
+    private IStringVectorizer vectorizerType;
+    
+    //holy boilerplate batman
+    public VectorizerThread(double[][] reference, int indexToModify, String string, String[] wordDictionary, IStringVectorizer vectorizer) {
+        this.reference = reference;
+        this.indexToModify = indexToModify;
+        this.string = string;
+        this.wordDictionary = wordDictionary;
+        this.vectorizerType = vectorizer;
+    }
+
+    // I hope this works 
+    @Override
+    public void run() {
+        this.reference[this.indexToModify] = this.vectorizerType.getVectorizedString(string, wordDictionary);
+    }
+    
+}

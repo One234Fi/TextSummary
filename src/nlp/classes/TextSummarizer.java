ca40ede0602bdd19ee2447d487081833bc5b12ae
One234Fi <ethan@DESKTOP-E030602>
2023-07-29 21:59:30 -0500

    added option flags to allow modification of the scoring thresholds, a flag parser function, and a help message

--- src/nlp/classes/TextSummarizer.java
+++ src/nlp/classes/TextSummarizer.java
@@ -6,6 +6,7 @@ package nlp.classes;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
@@ -49,7 +50,12 @@ public class TextSummarizer {
     /**
      * calls the methods to generate a summary
      */
-    public void start() {
+    public void start(HashMap<String, String> flags) {
+        double compositeThreshold = Double.parseDouble(flags.getOrDefault("-st", "1"));
+        double cst = Double.parseDouble(flags.getOrDefault("-cst", "1"));
+        double rst = Double.parseDouble(flags.getOrDefault("-rst", "1"));
+
+
         //find and read file
         String filePath = ui.pickFilePath();
         String contentText = fileReader.getText(filePath);
@@ -68,10 +74,14 @@ public class TextSummarizer {
         
         Map<String, int[]> contentVectors = stringVectorizer.getVectorizedData(contentSentences, wordDictionary);
         IMetric cosineSimilarity = new CosineSimilarity(contentVectors);
+
+        // a more robust solution should do this differently
         IMetric RAKE = new RAKERelevance(contentText);
-        StringScorer scorer = new StringScorer(contentVectors.keySet(), cosineSimilarity, RAKE);
+        double[] thresholds = {cst, rst};
+
+        StringScorer scorer = new StringScorer(contentVectors.keySet(), thresholds, cosineSimilarity, RAKE);
         
-        List<String> output = scorer.getTopStrings(0.4);
+        List<String> output = scorer.getTopStrings(compositeThreshold);
         for (String s : output) {
             System.out.println(s);
         }
